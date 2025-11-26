5a689349557acf49ef56a8ba9598c74fac2987db
Fernando D. Gómez Aranzabe <46504381+aranzabe@users.noreply.github.com>
2025-11-26 14:13:52 +0100

    cambios

--- factories/user_factory.cjs
+++ factories/user_factory.cjs
@@ -1,6 +1,21 @@
 const bcrypt = require('bcrypt');
 const { faker, fakerES } = require('@faker-js/faker');
 
+const  generarDNI = () => {
+  const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+  const numero = Math.floor(Math.random() * 100000000);
+  const letra = letras[numero % 23];
+  return numero.toString().padStart(4, '0') + letra;
+}
+
+const generarTelefono = () => {
+  // Móvil español entre 600000000 y 799999999
+  const num = faker.number.int({ min: 600000000, max: 799999999 });
+  const numStr = num.toString();
+  // Formato 999 999 999
+  return `${numStr.slice(0,3)} ${numStr.slice(3,6)} ${numStr.slice(6,9)}`;
+};
+
 const genUsers = async (ctos = 1) => {
 
     let usersGen = []
@@ -9,10 +24,12 @@ const genUsers = async (ctos = 1) => {
         const password = '1234';
         let u = 
             {
-            dni: fakerES.string.uuid(),
+            // dni: fakerES.string.uuid(),
+            dni: generarDNI(),
+            tfno: generarTelefono(),
             nombre: fakerES.person.fullName(),
             clave: password,
-            edad: Math.floor(Math.random() * 100) + 1,
+            edad: faker.number.int({ min: 18, max: 80 }),
             // createdAt: new Date(),
             // updatedAt: new Date()
             }
